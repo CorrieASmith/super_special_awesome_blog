f0c8bd15e532eb3e32ce2759ccf6787a4b94c0b4
Corrie <corriesmith87@gmail.com>
2015-10-16 17:12:05 -0700

    add migration for posts

---
+++ db/migrate/20151017000651_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.column :name, :string
+      t.column :content, :string
+
+      t.timestamps
+    end
+  end
+end

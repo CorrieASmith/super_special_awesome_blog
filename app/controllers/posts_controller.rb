dea956dcad71c633e0833627663610dc1c7a61a3
Corrie <corriesmith87@gmail.com>
2015-10-16 17:37:27 -0700

    add edit post functionality; partial for edit form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:name)

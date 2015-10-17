d568b2a289a979214ff49f3339529aebc7672056
Corrie <corriesmith87@gmail.com>
2015-10-16 17:29:16 -0700

    allow user to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,18 @@ class PostsController < ApplicationController
     @post = Post.new
     render :new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render:new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:name)
+  end
 end
